31ebdb0c7622df38a807e390dd3cd2f50049078e
Ahmad Sopian <sopianahmad120@gmail.com>
2023-04-08 14:09:04 +0700

    Include timber

--- app/src/main/java/com/sopian/movieaej/MovieAejApp.kt
+++ app/src/main/java/com/sopian/movieaej/MovieAejApp.kt
@@ -2,6 +2,16 @@ package com.sopian.movieaej
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class MovieAejApp : Application()
+class MovieAejApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
