fccc773bf706ab4ce10e6230e29728f6452d12a4
Ahmad Sopian <sopianahmad120@gmail.com>
2023-04-10 12:32:25 +0700

    Add Retrofit service interface for API communication

---
+++ app/src/main/java/com/sopian/movieaej/data/source/remote/network/MovieService.kt
@@ -0,0 +1,16 @@
+package com.sopian.movieaej.data.source.remote.network
+
+import com.sopian.movieaej.data.source.remote.response.BaseResponse
+import com.sopian.movieaej.data.source.remote.response.MovieResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieService {
+
+    @GET("movie/upcoming")
+    suspend fun getUpcomingMovies(
+        @Query("page") page: Int,
+        @Query("language") isoCode: String,
+        @Query("region") region: String
+    ): BaseResponse<MovieResponse>
+}
